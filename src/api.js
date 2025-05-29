f4035c433dc41825e2c0f20ea95fa59943504a75
Gabriel Saraiva <gabriel.saraiva.flooxai@gmail.com>
2025-05-29 01:53:50 -0300

    fix(api): update baseURL to include "/api" for backend route compatibility

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://helpheal-backend.onrender.com'
+  baseURL: 'https://helpheal-backend.onrender.com/api'
 })
 
 api.interceptors.request.use(config => {
